0e3ad9482c66656dfdf1cfc384515bbb511a74ec
General Zelgius <mattlow123@gmail.com>
2022-02-10 19:27:13 -0500

    finished setting up search button and historical search functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,14 +8,18 @@ var searchBtnHandler = function(event) {
     event.preventDefault();
 
     var city = document.querySelector("input[name='city']").value;
-
-    console.log(city);
-
+    console.log(cities);
+    
     if (!city) {
         alert("Please enter a city")
     }
-
-    createSearchEl();
+    
+    if (!cities.includes(city)) {
+        
+        createSearchEl();
+        cities.push(city)
+    }
+    
 }
 
 var createSearchEl = function() {
@@ -25,16 +29,37 @@ var createSearchEl = function() {
     btnItemEl.textContent = document.querySelector("input[name='city']").value
     btnItemEl.setAttribute("data-city-id", cityCounter);
     btnItemEl.setAttribute("id", "searchBtn");
-    btnItemEl.setAttribute("type", "submit");
-    
-    // cityDataObj.id = cityCounter;
-    // cities.push(cityDataObj)
+    btnItemEl.setAttribute("type", "submit");    
 
     citySearch.appendChild(btnItemEl);
 
     cityCounter++;
 
     saveSearch();
+}
+
+var loadSearch = function() {
+    
+    var searchHist = JSON.parse(localStorage.getItem("cities"));
+    console.log(searchHist);
+    for (var index = 0; index < searchHist.length; index++) {
+        
+    var btnItemEl = document.createElement("button");
+    btnItemEl.className = "btn btn-secondary btn-block";
+    btnItemEl.textContent = searchHist[index]
+    btnItemEl.setAttribute("data-city-id", cityCounter);
+    btnItemEl.setAttribute("id", "searchBtn");
+    btnItemEl.setAttribute("type", "submit");
+
+    citySearch.appendChild(btnItemEl);
+    }
+}
+
+if (localStorage.getItem("cities")) {
+    cities = JSON.parse(localStorage.getItem("cities"))
+}
+if (cities.length !== 0) {
+    loadSearch();
 }
 
 var saveSearch = function() {
