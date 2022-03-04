8d293877c18f7b4d7f131e786fd50be52200ad12
General Zelgius <mattlow123@gmail.com>
2022-03-04 17:05:20 -0500

    finished building five-day forecast and cleaned up the look a bit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -98,7 +98,7 @@ var displayResult = function(city, state) {
 }
 
 var weatherReport = function() {
-    var apiURL = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&appid=93ec56821a727ff38ffe9140f6060c88"
+    var apiURL = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=93ec56821a727ff38ffe9140f6060c88"
     fetch(apiURL)
         .then(function(response) {
             if(response.ok) {
@@ -120,6 +120,7 @@ var currentWeatherDisplay = function(cityData) {
     var iconIMG = "http://openweathermap.org/img/wn/" + weatherIconURL + "@2x.png";
     icon.innerHTML = "<img src=" + iconIMG + ">";
     
+
     temp.textContent = "Temp: " + cityData.current.temp + " °F";
 
     
@@ -128,6 +129,7 @@ var currentWeatherDisplay = function(cityData) {
 
     humidity.textContent = "Humidity: " + cityData.current.humidity + " %";
 
+    
     UV.textContent = "UV Index: " + cityData.current.uvi;
 
     bigWeather.appendChild(icon);
@@ -148,8 +150,61 @@ var fiveDayForecast = function(daily) {
     var day_hum = [];
     var day_icon = [];
 
-    
- 
+    for (var i = 0; i < daily.length; i++) {
+        var unix = daily[i].dt;
+        var temp = daily[i].temp.day;
+        var wind = daily[i].wind_speed;
+        var humid = daily[i].humidity;
+        var icon = daily[i].weather[0].icon;
+        
+        day[i] = moment.unix(unix);
+        day_temp[i] = temp;
+        day_wind[i] = wind;
+        day_hum[i] = humid;
+        day_icon[i] = icon;
+    }
+
+    var firstIcon = "http://openweathermap.org/img/wn/" + day_icon[1] + "@2x.png"
+    var secondIcon = "http://openweathermap.org/img/wn/" + day_icon[2] + "@2x.png"
+    var thirdIcon = "http://openweathermap.org/img/wn/" + day_icon[3] + "@2x.png"
+    var fourthIcon = "http://openweathermap.org/img/wn/" + day_icon[4] + "@2x.png"
+    var fifthIcon = "http://openweathermap.org/img/wn/" + day_icon[5] + "@2x.png"
+
+    firstDay.innerHTML = "<h3>" + moment(day[1]._d).format('LL') + "</h3>"
+        + "<img src=" + firstIcon + ">"
+        + "<li>" + "Temp: " + day_temp[1] + "°F" + "</li>"
+        + "<li>" + "Wind: " + day_wind[1] + "mph" + "</li>"
+        + "<li>" + "Humidity: " + day_hum[1] + "%" + "</li>"
+
+    secondDay.innerHTML = "<h3>" + moment(day[2]._d).format('LL') + "</h3>"
+        + "<img src=" + secondIcon + ">"
+        + "<li>" + "Temp: " + day_temp[2] + "°F" + "</li>"
+        + "<li>" + "Wind: " + day_wind[2] + "mph" + "</li>"
+        + "<li>" + "Humidity: " + day_hum[2] + "%" + "</li>"
+
+    thirdDay.innerHTML = "<h3>" + moment(day[3]._d).format('LL') + "</h3>"
+        + "<img src=" + thirdIcon + ">"
+        + "<li>" + "Temp: " + day_temp[3] + "°F" + "</li>"
+        + "<li>" + "Wind: " + day_wind[3] + "mph" + "</li>"
+        + "<li>" + "Humidity: " + day_hum[3] + "%" + "</li>"
+
+    fourthDay.innerHTML = "<h3>" + moment(day[4]._d).format('LL') + "</h3>"
+        + "<img src=" + fourthIcon + ">"
+        + "<li>" + "Temp: " + day_temp[4] + "°F" + "</li>"
+        + "<li>" + "Wind: " + day_wind[4] + "mph" + "</li>"
+        + "<li>" + "Humidity: " + day_hum[4] + "%" + "</li>"
+
+    fifthDay.innerHTML = "<h3>" + moment(day[5]._d).format('LL') + "</h3>"
+        + "<img src=" + fifthIcon + ">"
+        + "<li>" + "Temp: " + day_temp[5] + "°F" + "</li>"
+        + "<li>" + "Wind: " + day_wind[5] + "mph" + "</li>"
+        + "<li>" + "Humidity: " + day_hum[5] + "%" + "</li>"
+
+    day1.appendChild(firstDay);
+    day2.appendChild(secondDay);
+    day3.appendChild(thirdDay);
+    day4.appendChild(fourthDay);
+    day5.appendChild(fifthDay);
 
 }
 
