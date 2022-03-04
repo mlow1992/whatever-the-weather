e76768fe97592bb6f5f31dc0d44ae83ebfe61125
General Zelgius <mattlow123@gmail.com>
2022-03-04 11:57:36 -0500

    reformatted html and began api calls in script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,14 @@
 var searchEl = document.querySelector("#city");
 var searchBtn = document.querySelector("#searchBtn");
 var citySearch = document.querySelector("#city-search");
+var currentWeather = document.querySelector("current-weather");
+var fiveDay = document.querySelector("weather-cards");
+
+var day1 = document.querySelector("day_1");
+var day2 = document.querySelector("day_2");
+var day3 = document.querySelector("day_3");
+var day4 = document.querySelector("day_4");
+var day5 = document.querySelector("day_5");
 var cityCounter = 0;
 var cities = []
 
@@ -10,7 +18,10 @@ var searchBtnHandler = function(event) {
     var city = document.querySelector("input[name='city']").value;
     console.log(cities);
     
-    if (!city) {
+    if (city) {
+        getWeatherData(city);
+
+    } else {
         alert("Please enter a city")
     }
     
@@ -36,6 +47,33 @@ var createSearchEl = function() {
     cityCounter++;
 
     saveSearch();
+}
+
+var getWeatherData = function(city, state) {
+    var apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + ", " + state + "&limit=5&appid=93ec56821a727ff38ffe9140f6060c88";
+    fetch(apiURL)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    displayResult(data);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function(error) {
+            alert("Unable to connect")
+        });
+};
+
+var displayResult = function(city, state) {
+    if (city.length === 0) {
+        alert("city not found");
+        return;
+    }
+    if (!state) {
+        alert("please specify a state")
+    }
 }
 
 var loadSearch = function() {
